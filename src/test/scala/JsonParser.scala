ce94dcb377c00b97f414c988f01d83dbe96f1bab
Victor Dubé <victor.dube@agilogy.com>
2022-11-22 12:41:06 +0100

    finish to parse array of booleans

--- src/test/scala/JsonParser.scala
+++ src/test/scala/JsonParser.scala
@@ -1,15 +1,20 @@
 package com.agilogy.wapl
 
-import Json.JsonArray
+import Json.{JsonArray, JsonBoolean}
+import scala.language.postfixOps
 
 object JsonParser:
 
   private val whiteSpaceChars = Set(' ', '\n', '\r', '\t')
 
-  val whitespace: Parser[Int] = (s, position) =>
-    Right(position + s.substring(position).takeWhile(c => whiteSpaceChars.contains(c)).length)
+  val whitespace: Parser[Unit] = (s, position) =>
+    Right(position + s.substring(position).takeWhile(c => whiteSpaceChars.contains(c)).length, ())
 
+  val trueBoolean: Parser[JsonBoolean] = string("true").map(_ => JsonBoolean(true))
+  val falseBoolean: Parser[JsonBoolean] = string("false").map(_ => JsonBoolean(false))
+  val boolean: Parser[JsonBoolean] = trueBoolean | falseBoolean
+
+  val arrayItems: Parser[List[JsonBoolean]] = ((boolean <* whitespace <* string(",") <* whitespace) | boolean)*
   val array: Parser[JsonArray] =
-    (string("[") ** whitespace ** string("]"))
-      .map(_ => JsonArray(List.empty))
-
+    (string("[") *> whitespace *> arrayItems <* whitespace <* string("]"))
+      .map(JsonArray)
--- src/test/scala/JsonParserTest.scala
+++ src/test/scala/JsonParserTest.scala
@@ -2,6 +2,7 @@ package com.agilogy.wapl
 
 import Json.*
 
+import com.agilogy.wapl.JsonParser.array
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.util.Random
@@ -11,14 +12,27 @@ class JsonParserTest extends AnyFunSuite :
   import JsonParser.*
 
   test("Parse empty array") {
-    assert(array("[]", 0) == Right(JsonArray(List.empty)))
+    assert(array("[]", 0) == Right((2,JsonArray(List.empty))))
   }
 
   private val random = new Random()
 
+  test("Parse boolean") {
+    val bool = random.nextBoolean()
+    assert(boolean(bool.toString, 0) == Right((bool.toString.length, JsonBoolean(bool))))
+  }
+
+  test("Parse array of booleans") {
+    assert(array("[true,false,true]", 0) == Right((17,JsonArray(List(JsonBoolean(true),JsonBoolean(false),JsonBoolean(true))))))
+  }
+
+  test("Parse array of booleans with spaces") {
+    assert(array("[ true, false, true ]", 0) == Right((21,JsonArray(List(JsonBoolean(true),JsonBoolean(false),JsonBoolean(true))))))
+  }
+
   test("Parse empty array with whitespace") {
     val ws = " " * random.between(1, 5)
-    assert(array(s"[$ws]", 0) == Right(JsonArray(List.empty)))
+    assert(array(s"[$ws]", 0) == Right((2 + ws.length, JsonArray(List.empty))))
   }
 
   test("Parse empty array failure") {
@@ -27,7 +41,7 @@ class JsonParserTest extends AnyFunSuite :
   }
 
   test("Parse token") {
-    assert(string("[")("[", 0) == Right(1))
+    assert(string("[")("[", 0) == Right((1, ())))
   }
 
   test("Parse token failure") {
